ec59e38bdfb6464fe06da8ffcc6a09dd45b737cc
desmondsimzh <desmondsimzh@gmail.com>
2019-11-29 17:16:54 +0800

    Remove TeachListPage.js , rename LearnListPage.jsto LessonListPage.js (second attempt)

---
+++ frontend/next-project/src/pages/LessonListPage.js
@@ -0,0 +1,40 @@
+/* Import package components */
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+/* Import app components */
+import SearchBar from "../components/SearchBar";
+import ListCard from "../components/ListCard";
+import { getApiRoute } from "../global";
+
+export default function LessonListPage({props, teach}) {
+  const [lessonsData, setLessonsData] = useState({
+    datas: []
+  });
+
+  useEffect(() => {
+    axios
+      .get(`${getApiRoute("lessons/")}`, {teach:teach})
+      .then(result => {
+        console.log(result.data);
+        setLessonsData({
+          datas: result.data
+        });
+      })
+      .catch(error => {
+        console.log("ERROR: ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div style={{ width: "100vw" }}>
+        <SearchBar />
+      </div>
+      <div style={{ marginTop: "10px", display: "grid", justifyContent: "center" }} id="cardBox">
+        {lessonsData.datas.map(lesson => 
+          <ListCard lesson={lesson} key={lesson.id}/>
+        )}
+      </div>
+    </div>
+  );
+}
